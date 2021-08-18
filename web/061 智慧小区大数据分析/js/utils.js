6b6250bef777f09f6da65ff529a6d89c1054ace2
高伟 <1390903070@qq.com>
2021-08-18 13:54:19 +0800

    更新web061html

---
+++ web/061 智慧小区大数据分析/js/utils.js
@@ -0,0 +1,15 @@
+
+//生成从minNum到maxNum的随机数
+function randomNum(minNum, maxNum) {
+    switch (arguments.length) {
+        case 1:
+            return parseInt(Math.random() * minNum + 1, 10);
+            break;
+        case 2:
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
