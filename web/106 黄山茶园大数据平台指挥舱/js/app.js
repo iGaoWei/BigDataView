fc9ccde2777d6b51ae5232c6260938d2f50e7884
GAOWEI <1390903070@qq.com>
2023-03-30 11:23:42 +0800

    add web 106

---
+++ web/106 黄山茶园大数据平台指挥舱/js/app.js
@@ -0,0 +1,53 @@
+/* default dom id (particles-js) */
+//particlesJS();
+
+/* config dom id */
+//particlesJS('dom-id');
+
+/* config dom id (optional) + config particles params */
+particlesJS('particles-js', {
+  particles: {
+    color: '#0ff',
+    shape: 'circle', // "circle", "edge" or "triangle"
+    opacity: .5,
+    size: 4,
+    size_random: true,
+    nb: 50,
+    line_linked: {
+      enable_auto: true,
+      distance: 100,
+      color: '#fff',
+      opacity: 0.1,
+      width: .5,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    enable: true,
+    mouse: {
+      distance: 300
+    },
+    detect_on: 'canvas', // "canvas" or "window"
+    mode: 'grab',
+    line_linked: {
+      opacity: .1
+    },
+    events: {
+      onclick: {
+        enable: true,
+        mode: 'push', // "push" or "remove"
+        nb: 4
+      }
+    }
+  },
+  /* Retina Display Support */
+  retina_detect: true
+});
